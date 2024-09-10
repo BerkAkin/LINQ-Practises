a8ab5a33714559e1388d2f10a89b326a05cace90
Berk Akın <berkakin99@hotmail.com>
2024-09-10 04:42:25 +0300

    cache update after build

--- obj/Debug/net8.0/LinqConsoleApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/LinqConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LinqConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a214489d10cce16f292cb79138aa0fedcfec15c7")]
 [assembly: System.Reflection.AssemblyProductAttribute("LinqConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LinqConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
